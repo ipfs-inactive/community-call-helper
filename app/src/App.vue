da2c6bb5a3f8131016f02e685c4f36e305802c9e
Portia Burton <plburton@gmail.com>
2018-12-20 15:45:55 +0100

    Accepts and retunrs user input

--- app/src/App.vue
+++ app/src/App.vue
@@ -2,17 +2,20 @@
   <div id="app">
     <NavBar />
     <HomePage />
+    <ScheduledCalls />
   </div>
 </template>
 
 <script>
 import NavBar from './components/NavBar.vue'
 import HomePage from './components/HomePage.vue'
+import ScheduledCalls from './components/ScheduledCalls.vue'
 export default {
   name: 'app',
   components: {
     NavBar,
-    HomePage
+    HomePage,
+    ScheduledCalls
   }
 }
 </script>
--- app/src/components/HomePage.vue
+++ app/src/components/HomePage.vue
@@ -1,13 +1,14 @@
 <template>
     <div class="hompage">
-        <h1 class="center">{{ msg }}</h1>
+        <h1 class="center">Community Call Utils</h1>
         <main class="pa4 black-80">
             <form class="measure center">
                 <fieldset id="a" class="ba b--transparent ph0 mh0">
                 <legend class="f4 fw6 ph0 mh0">Broadcast Community Call 🎉🎊🎉 </legend>
                 <div class="mt3">
-                    <label class="db fw6 lh-copy f6" for="email-address">Name of Working Group</label>
-                    <input class="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address">
+                    <label class="db fw6 lh-copy f6" for="working-group">Name of Working Group</label>
+                    <input class="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="textfield" name="working-group"  id="working-group" v-model="workingGroup">
+                        {{ workingGroup }}
                 </div>
                 <div class="mv3">
                     <label class="db fw6 lh-copy f6" for="password">Name of Call</label>
@@ -17,10 +18,9 @@
                     <label class="db fw6 lh-copy f6" for="password">Announcment</label>
                     <input class="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password">
                 </div>
-
                 </fieldset>
                 <div class="">
-                <input class="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Submit">
+                <input class="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib popup" type="submit" value="Submit" v-on:click.prevent="attention">
                 </div>
             </form>
         </main>    
@@ -29,9 +29,10 @@
 <script>
 export default {
   name: 'HomePage',
-  data() {
-      return {
-          msg: "Commmunity Call Utils",
+  methods: {
+      attention: function(event) {
+          event.preventDefault();
+          alert(this.workingGroup)
       }
   }
 }
