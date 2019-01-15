2791ef23b8be0b45a452730f9da5734f6b192558
Mikeal Rogers <mikeal.rogers@gmail.com>
2019-01-15 14:16:52 -0800

    fix: vuejs data needs to be initialized and form auto-posts

--- app/src/components/HomePage.vue
+++ app/src/components/HomePage.vue
@@ -2,13 +2,12 @@
     <div class="hompage">
         <h1 class="center">Community Call Utils</h1>
         <main class="pa4 black-80">
-            <form class="measure center">
+            <div class="measure center">
                 <fieldset id="a" class="ba b--transparent ph0 mh0">
                 <legend class="f4 fw6 ph0 mh0">Broadcast Community Call 🎉🎊🎉 </legend>
                 <div class="mt3">
                     <label class="db fw6 lh-copy f6" for="working-group">Name of Working Group</label>
                     <input class="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="textfield" name="working-group"  id="working-group" v-model="workingGroup">
-                        {{ workingGroup }}
                 </div>
                 <div class="mv3">
                     <label class="db fw6 lh-copy f6" for="textfield">Name of Call</label>
@@ -16,14 +15,14 @@
                 </div>
                 <div class="mv3">
                     <label class="db fw6 lh-copy f6" for="textfield">Announcment</label>
-                    <input class="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="textfield" name="textfield"  id="textfield">
+                    <input class="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="textfield" name="textfield"  id="textfield" v-model="announcement">
                 </div>
                 </fieldset>
                 <div class="">
                 <input class="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib popup" type="submit" value="Submit" @click="createPost()">
                 </div>
-            </form>
-        </main>    
+            </div>
+        </main>
     </div>
 </template>
 <script>
@@ -31,15 +30,19 @@ import axios from 'axios'
 
 export default {
   name: 'HomePage',
+  data: () => ({
+    callName: null,
+    workingGroup: null,
+    announcement: null
+  }),
   methods: {
-    createPost() {
-        axios.post('https://api.github.com/repos/pkafei/Distributed-Medicine/issues?access_token=key', {
-        title: this.workingGroup,
-        body: this.callName
-    }).then((response) => {})
-    .catch((e) => {
-        console.error(e)
-        })
+    async createPost() {
+      let url = `https://api.github.com/repos/pkafei/Distributed-Medicine/issues?access_token=${localStorage.token}`
+      let response = await axios.post(url, {
+          title: this.workingGroup,
+          body: this.callName
+      })
+      console.log(response)
     }
   }
 }
