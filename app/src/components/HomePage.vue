0002f3d4dc583d6c0e758a78a5ea6f2bdcf03849
Portia Burton <plburton@gmail.com>
2019-01-17 17:20:16 +0100

    Added Date, Topic, and Presenter fields and capability to parse HTML

--- app/src/components/HomePage.vue
+++ app/src/components/HomePage.vue
@@ -6,16 +6,16 @@
                 <fieldset id="a" class="ba b--transparent ph0 mh0">
                 <legend class="f4 fw6 ph0 mh0">Broadcast Community Call 🎉🎊🎉 </legend>
                 <div class="mt3">
-                    <label class="db fw6 lh-copy f6" for="sync-date">Date of Community Sync</label>
+                    <label class="db fw6 lh-copy f6" for="sync-date">Date</label>
                     <input class="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="textfield" name="sync-date"  id="sync-date" v-model="syncDate">
                 </div>
                 <div class="mv3">
-                    <label class="db fw6 lh-copy f6" for="textfield">Name of Call</label>
-                    <input class="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="textfield" name="textfield"  id="textfield" v-model="callName">
+                    <label class="db fw6 lh-copy f6" for="textfield">Presenter</label>
+                    <input class="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="textfield" name="textfield"  id="textfield" v-model="presenter">
                 </div>
                 <div class="mv3">
-                    <label class="db fw6 lh-copy f6" for="textfield">Announcment</label>
-                    <input class="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="textfield" name="textfield"  id="textfield" v-model="announcement">
+                    <label class="db fw6 lh-copy f6" for="textfield">Topic</label>
+                    <input class="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="textfield" name="textfield"  id="textfield" v-model="topic">
                 </div>
                 </fieldset>
                 <div class="">
@@ -29,22 +29,28 @@
 export default {
   name: 'HomePage',
   data: () => ({
-    callName: null,
     syncDate: null,
-    announcement: null
+    presenter: null,
+    topic: null
   }),
   methods: {
     async createPost() {
       let url = `https://api.github.com/repos/pkafei/Distributed-Medicine/issues?access_token=01f8414215a5942c6984ff9c8c39df50751b5679`
+      let call_date = this.syncDate
+      let agenda = "<h1>Agenda and Notes</h1>"
+      let agenda_items = `Please add items before the call.`
+      let call_details = "<table><td>Insert Call Details Here</td></table><br>"
+      let topic = "\n Topic: "
+
       const rawResponse = await fetch(url, {
         method: 'POST',
         headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
+        'Accept': 'application/vnd.github.v3.html+json',
+        'Content-Type': 'application/vnd.github.v3.html'
         },
         body: JSON.stringify({
-          title: this.syncDate,
-          body: this.callName
+          title: `IPFS Weekly Call ${this.syncDate} ⚡️ 📞`,
+          body: `${agenda_items} \n \n` + call_details + `Presenter: ` + this.presenter + topic
         })
       })
       const content = await rawResponse.json();
